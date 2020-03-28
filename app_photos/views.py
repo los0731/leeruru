16d7cd52fceddc782979081e479bce5946b6df84
los0731 <los0731@gmail.com>
2020-03-28 15:29:49 +0900

    pagenation setting

--- app_photos/views.py
+++ app_photos/views.py
@@ -1,33 +1,42 @@
 from django.shortcuts import render
 import os
-import random
 
 # Create your views here.
 # ↓↓↓ 추가
-img_0 = "img_ruru_0.jpg"
-my_name = "내 이름은 부랑구야."
+
+# 로컬을 위한 경로.
+imgs_path = '/Users/frank.io/Documents/workplace/django_4th/app_photos/static/imgs'
+# pythonanywhere용 절대 경로.
+# imgs_path = '/home/leeruru/leeruru/app_photos/static/imgs'
+
 list_imgs_src = []
-
-current_path = os.getcwd()
-imgs_path = 'app_photos/static/imgs'
-
 list_imgs = os.listdir(imgs_path)
-# # print(list_imgs)
 
 
-for i in list_imgs:
-    tag = '{}{}'.format('/static/imgs/', i)
+for idx, val in enumerate(list_imgs):
+    tag = '/static/imgs/{}'.format(val)
     list_imgs_src.append(tag)
-    print(tag)
 
-
+page_1 = list_imgs_src[0:199]
+page_2 = list_imgs_src[200:399]
+page_3 = list_imgs_src[400:599]
+page_4 = list_imgs_src[600:799]
+page_5 = list_imgs_src[800:999]
+page_6 = list_imgs_src[1000:1199]
+page_7 = list_imgs_src[1200:1399]
+page_8 = list_imgs_src[1400:]
 
 data = {
-    # 'list_img': list_imgs,
     'list_imgs_src': list_imgs_src,
-    'img_0': img_0,
+    'page_1': page_1,
+    'page_2': page_2,
+    'page_3': page_3,
+    'page_4': page_4,
+    'page_5': page_5,
+    'page_6': page_6,
+    'page_7': page_7,
+    'page_8': page_8,
 }
-
 
 def photos_album(request):
     return render(request, 'app_photos/index.html', data)
