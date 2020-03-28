a2223afa58b7c2b2d5552d0064fe26c5735d4ee3
los0731 <los0731@gmail.com>
2020-03-28 15:08:40 +0900

    photos update

--- app_photos/views.py
+++ app_photos/views.py
@@ -1,12 +1,31 @@
 from django.shortcuts import render
+import os
+import random
 
 # Create your views here.
 # ↓↓↓ 추가
-say_hi = "안녕 하하핳?"
+img_0 = "img_ruru_0.jpg"
 my_name = "내 이름은 부랑구야."
+list_imgs_src = []
+
+current_path = os.getcwd()
+imgs_path = 'app_photos/static/imgs'
+
+list_imgs = os.listdir(imgs_path)
+# # print(list_imgs)
+
+
+for i in list_imgs:
+    tag = '{}{}'.format('/static/imgs/', i)
+    list_imgs_src.append(tag)
+    print(tag)
+
+
+
 data = {
-    'say_hi': say_hi,
-    'my_name': my_name
+    # 'list_img': list_imgs,
+    'list_imgs_src': list_imgs_src,
+    'img_0': img_0,
 }
 
 
